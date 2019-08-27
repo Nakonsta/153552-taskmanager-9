d3bf4d38333a436b6c465658ca2a2313a3a05b89
Анастасия <na.konsta@gmail.com>
2019-08-27 07:11:51 +0300

    Components - from functions to objects

--- src/components/taskCard.js
+++ src/components/taskCard.js
@@ -10,6 +10,13 @@ class Task {
     this._isArchive = isArchive;
   }
 
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
   getTemplate() {
     return `<article class="card card--${this._color} ${Object.values(this._repeatingDays).some((it) => it === true) ? `card--repeat` : ``}">
             <div class="card__form">
@@ -67,59 +74,4 @@ class Task {
   }
 }
 
-const getTaskCardTemplate = ({description, dueDate, repeatingDays, tags, color, isFavorite, isArchive}) => {
-  return `<article class="card card--${color} ${Object.keys(repeatingDays).some((day) => repeatingDays[day]) ? `card--repeat` : ``}">
-            <div class="card__form">
-            <div class="card__inner">
-                <div class="card__control">
-                <button type="button" class="card__btn card__btn--edit">
-                    edit
-                </button>
-                ${isArchive ? `<button type="button" class="card__btn card__btn--archive">
-                    archive
-                </button>` : ``}
-                ${isFavorite ? ` <button
-                    type="button"
-                    class="card__btn card__btn--favorites card__btn--disabled"
-                >
-                    favorites
-                </button>` : ``}
-                </div>
-
-                <div class="card__color-bar">
-                <svg class="card__color-bar-wave" width="100%" height="10">
-                    <use xlink:href="#wave"></use>
-                </svg>
-                </div>
-
-                <div class="card__textarea-wrap">
-                <p class="card__text">${description}</p>
-                </div>
-
-                <div class="card__settings">
-                <div class="card__details">
-                    <div class="card__dates">
-                    <div class="card__date-deadline">
-                        <p class="card__input-deadline-wrap">
-                        <span class="card__date">${new Date(dueDate).toDateString()}</span>
-                        <span class="card__time"${new Date(dueDate).toTimeString()}</span>
-                        </p>
-                    </div>
-                    </div>
-
-                    <div class="card__hashtag">
-                    <div class="card__hashtag-list">
-                        ${Array.from(tags).map((tag) => `<span class="card__hashtag-inner">
-                            <span class="card__hashtag-name">
-                                #${tag}
-                            </span>
-                            </span>`).join(``)}
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>
-            </div>
-        </article>`.trim();
-};
-export {getTaskCardTemplate};
+export {Task};
