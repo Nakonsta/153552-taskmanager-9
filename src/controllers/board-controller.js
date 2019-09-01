b24d2435a851063178df5de978b5c5cb20e820b1
Анастасия <na.konsta@gmail.com>
2019-09-01 14:02:28 +0300

    Sort and edit button bug fixing

--- src/controllers/board-controller.js
+++ src/controllers/board-controller.js
@@ -2,6 +2,7 @@ import {Board} from '../components/board.js';
 import {TaskList} from '../components/taskList.js';
 import {Task} from '../components/taskCard.js';
 import {TaskEdit} from '../components/taskEdit.js';
+import {Sort} from '../components/sort.js';
 import {render, Positions} from '../utils.js';
 
 class BoardController {
@@ -9,13 +10,20 @@ class BoardController {
     this._container = container;
     this._tasks = tasks;
     this._board = new Board();
+    this._sort = new Sort();
     this._taskList = new TaskList();
   }
 
   init() {
     render(this._container, this._board.getElement(), Positions.BEFOREEND);
+    render(this._board.getElement(), this._sort.getElement(), Positions.AFTERBEGIN);
     render(this._board.getElement(), this._taskList.getElement(), Positions.BEFOREEND);
+
     this._tasks.forEach((taskMock) => this._renderTask(taskMock));
+
+    this._sort.getElement().addEventListener(`click`, (evt) => {
+      this._onSortClick(evt);
+    });
   }
 
   _renderTask(task) {
@@ -30,7 +38,7 @@ class BoardController {
     };
 
     taskComponent.getElement().querySelector(`.card__btn--edit`).addEventListener(`click`, () => {
-      this._taskList.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+      this._taskList.getElement().replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
       document.addEventListener(`keydown`, onEscKeyDown);
     });
 
@@ -43,11 +51,37 @@ class BoardController {
     });
 
     taskEditComponent.getElement().querySelector(`.card__save`).addEventListener(`click`, () => {
-      this._taskList.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+      this._taskList.getElement().replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
       document.removeEventListener(`keydown`, onEscKeyDown);
     });
 
     render(this._taskList.getElement(), taskComponent.getElement(), Positions.BEFOREEND);
+  }
+
+  _onSortClick(evt) {
+    evt.preventDefault();
+
+    
+
+    if (evt.target.tagName !== `A`) {
+      return;
+    }
+
+    this._taskList.getElement().innerHTML = ``;
+
+    switch (evt.target.dataset.sortType) {
+      case `date-up`:
+        const sortedByDateUpTasks = this._tasks.slice().sort((a, b) => a.dueDate - b.dueDate);
+        sortedByDateUpTasks.forEach((taskMock) => this._renderTask(taskMock));
+        break;
+      case `date-down`:
+        const sortedByDateDownTasks = this._tasks.slice().sort((a, b) => b.dueDate - a.dueDate);
+        sortedByDateDownTasks.forEach((taskMock) => this._renderTask(taskMock));
+        break;
+      case `default`:
+        this._tasks.forEach((taskMock) => this._renderTask(taskMock));
+        break;
+    }
   }
 }
 
